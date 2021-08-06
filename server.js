79f648fa7f21da33275845a4b45aa41d51483cea
NakshatraCodes <nakshatra163@gmail.com>
2021-08-07 02:49:30 +0530

    Swagger API Docs

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const fastify = require('fastify')({ logger: true });
 const PORT = 5000;
 
+fastify.register(require('fastify-swagger'), {
+  exposeRoute: true,
+  routePrefix: '/api/docs',
+  swagger: {
+    info: { title: 'fastify-example-api' },
+  },
+});
 fastify.register(require('./routes/users'));
 
 const startServer = async () => {
